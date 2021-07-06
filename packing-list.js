7c2a4a2a4187f4862875227ae3fbe9c4814eb886
Tharsikan <thar.navaratnam@gmail.com>
2021-07-06 19:47:14 +0000

    Added while loop example

--- packing-list.js
+++ packing-list.js
@@ -1,8 +1,19 @@
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
+//Iterating over an Array with for loop
+
+
 console.log("Kitchen stuff to pack:");
 for (let i = 0; i < packingList.length; i++) {
   console.log(packingList[i]);
 }
 
+//Iterating over an Array with while loop
+
+console.log("Kitchen stuff to pack:");
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
+}
 
