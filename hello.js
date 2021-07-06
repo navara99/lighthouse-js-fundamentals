a59ebe8379c5e6e7aaf08894baab7ba1a4435263
Tharsikan <thar.navaratnam@gmail.com>
2021-07-06 17:31:06 +0000

    Adding hello.js file

---
+++ hello.js
@@ -0,0 +1,15 @@
+/*
+const sayHello  = function (hello) {
+  console.log(`${hello}, world`);
+}
+
+sayHello(`hello`);
+*/
+
+const sayHello  = function (name) {
+  console.log("Hello, " + name);
+}
+
+sayHello("Caliban");
+sayHello("Miranda");
+sayHello("Ferdinand");
